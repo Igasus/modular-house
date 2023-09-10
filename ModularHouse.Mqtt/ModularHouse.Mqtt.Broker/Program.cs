1055abad629905843f7f8060e439ce8c3725396f
Igasus <ihor.hubskyi@gmail.com>
2023-09-10 22:37:50 +0300

    (feat): Initial commit.

---
+++ ModularHouse.Mqtt/ModularHouse.Mqtt.Broker/Program.cs
@@ -0,0 +1,16 @@
+using ModularHouse.Mqtt.Broker;
+using MQTTnet.AspNetCore;
+
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder =>
+    {
+        webBuilder.UseKestrel(options =>
+            options.ListenAnyIP(1883, listenOptions => listenOptions.UseMqtt()));
+
+        webBuilder.UseStartup<Startup>();
+    });
+
+var app = builder.Build();
+
+
+app.Run();
---
+++ ModularHouse.Mqtt/ModularHouse.Mqtt.Broker/Startup.cs
@@ -0,0 +1,12 @@
+﻿namespace ModularHouse.Mqtt.Broker;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+    }
+}
