7548e3ee55d0d234936b6a70ab85600d018aeb2c
Ihor Hubskyi <ihor.hubskyi@gmail.com>
2023-09-11 21:34:26 +0300

    Entity Framework Implementation. (#1)
    (feat): Connected SQL Server database.

---
+++ ModularHouse.Server/ModularHouse.Server.Domain/UserAggregate/User.cs
@@ -0,0 +1,8 @@
+﻿namespace ModularHouse.Server.Domain.UserAggregate;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+}
---
+++ ModularHouse.Server/ModularHouse.Server.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModularHouse.Server.Domain.UserAggregate;
+
+namespace ModularHouse.Server.Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly ModularHouseContext _dbContext;
+
+    public DbSet<User> Users => _dbContext.Users;
+    public DbContext DbContext => _dbContext;
+
+    public UserRepository(ModularHouseContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+}
