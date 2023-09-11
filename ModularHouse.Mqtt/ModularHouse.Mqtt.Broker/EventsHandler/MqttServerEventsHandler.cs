10dabfa5972b294f9dcb7a76b2f66b01711faefd
Igasus <ihor.hubskyi@gmail.com>
2023-09-11 23:09:55 +0300

    (fix): Removed unnecessary indents.

--- ModularHouse.Mqtt/ModularHouse.Mqtt.Broker/EventsHandler/MqttServerEventsHandler.cs
+++ ModularHouse.Mqtt/ModularHouse.Mqtt.Broker/EventsHandler/MqttServerEventsHandler.cs
@@ -21,42 +21,36 @@ public class MqttServerEventsHandler : IMqttServerEventsHandler
     public Task StartedAsync(EventArgs args)
     {
         _logger.LogInformation("MQTT Server started.");
-
         return Task.CompletedTask;
     }
 
     public Task StoppedAsync(EventArgs args)
     {
         _logger.LogInformation("MQTT Server stopped.");
-
         return Task.CompletedTask;
     }
 
     public Task ClientConnectedAsync(ClientConnectedEventArgs args)
     {
         _logger.LogInformation($"Client {args.ClientId} just connected.");
-
         return Task.CompletedTask;
     }
 
     public Task ClientDisconnectedAsync(ClientDisconnectedEventArgs args)
     {
         _logger.LogInformation($"Client {args.ClientId} just disconnected.");
-
         return Task.CompletedTask;
     }
 
     public Task ClientSubscribedTopicAsync(ClientSubscribedTopicEventArgs args)
     {
         _logger.LogInformation($"Client {args.ClientId} just subscribed topic {args.TopicFilter.Topic}.");
-
         return Task.CompletedTask;
     }
 
     public Task ClientUnsubscribedTopicAsync(ClientUnsubscribedTopicEventArgs args)
     {
         _logger.LogInformation($"Client {args.ClientId} just unsubscribed topic {args.TopicFilter}.");
-
         return Task.CompletedTask;
     }
 
