3538c94c845a6fe8f784c1a44231b4d3c70cddab
v.yusupov <v.yusupov@upswot.com>
2023-10-03 12:40:18 +0300

    -(feat): Initial commit.

---
+++ DeviceManagement.Api/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen()
+    .AddControllers();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
