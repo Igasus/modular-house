87833552eaba17673237d947b416515ab42bab16
Vladislawv <87234450+Vladislawv@users.noreply.github.com>
2023-10-15 20:30:41 +0300

    Set up PostgreSQL database (#8)
    (feat): Set up PostgreSQL database context using EntityFramework Core;
    (feat): Implemented auto-migration while starting the application;
    (feat): Set up user-secrets for DeviceManagement.Api project;
    (feat): Moved application startup from DeviceManagement.Api to the DeviceManagement.Startup project.

--- Server/DeviceManagement/DeviceManagement.Api/Controllers/PingController.cs
+++ Server/DeviceManagement/DeviceManagement.Api/Controllers/PingController.cs
@@ -1,4 +1,4 @@
-using System.Net.Mime;
+﻿using System.Net.Mime;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
---
+++ Server/DeviceManagement/DeviceManagement.Infrastructure/AssemblyConfigurator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ModularHouse.Server.DeviceManagement.Infrastructure.DataAccess.Database;
+
+namespace ModularHouse.Server.DeviceManagement.Infrastructure;
+
+public static class AssemblyConfigurator
+{
+    private const string POSTGRE_SQL_SERVER = "ModularHousePostgreSqlServer";
+    
+    public static IServiceCollection ConfigureInfrastructureServices(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<PostgreSqlContext>(options => 
+            options.UseNpgsql(configuration.GetConnectionString(POSTGRE_SQL_SERVER)));
+
+        return services;
+    }
+
+    public static IServiceProvider InitializeDatabase(this IServiceProvider services)
+    {
+        using var scope = services.CreateScope();
+        var postgreSqlContext = scope.ServiceProvider.GetRequiredService<PostgreSqlContext>();
+        postgreSqlContext.Database.Migrate();;
+
+        return services;
+    }
+}
