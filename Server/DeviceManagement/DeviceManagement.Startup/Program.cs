bfe9146c1540467af293f9a02f91a59a422fa786
v.yusupov <v.yusupov@upswot.com>
2023-10-24 12:44:32 +0300

    (feat): Set up internal messaging to the DeviceManagement using DomainEventBus.

--- Server/DeviceManagement/DeviceManagement.Startup/Program.cs
+++ Server/DeviceManagement/DeviceManagement.Startup/Program.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using ModularHouse.Server.DeviceManagement.Api;
+using ModularHouse.Server.DeviceManagement.Domain;
 using ModularHouse.Server.DeviceManagement.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .ConfigureWebApiServices()
-    .ConfigureInfrastructureServices(builder.Configuration);
+    .ConfigureInfrastructureServices(builder.Configuration)
+    .ConfigureDomainServices();
 
 var app = builder.Build();
 
