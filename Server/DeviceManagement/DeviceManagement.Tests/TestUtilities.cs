91a02dd4fa8be298f5bd8b8a7cc45de492ea3eb9
v.yusupov <v.yusupov@upswot.com>
2023-10-24 13:55:48 +0300

    (feat): Created DeviceManagement.Tests project for tests using in-memory database initialization.

---
+++ Server/DeviceManagement/DeviceManagement.Tests/TestUtilities.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModularHouse.Server.DeviceManagement.Infrastructure.DataAccess.Database;
+
+namespace DeviceManagement.Tests;
+
+public static class TestUtilities
+{
+    public static DbContext GetDatabaseContext()
+    {
+        var options = new DbContextOptionsBuilder<PostgreSqlContext>()
+            .UseInMemoryDatabase(databaseName: "InMemoryDatabase")
+            .Options;
+
+        return new PostgreSqlContext(options);
+    }
+}
