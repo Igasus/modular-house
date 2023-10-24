198517847f473c8c7b2a0d292fdf42cb46ca100b
Vladislawv <87234450+Vladislawv@users.noreply.github.com>
2023-10-24 12:58:59 +0300

    Set up CQRS messaging (#15)
    (feat): Set up CQRS messaging in the DeviceManagement.Application project.

---
+++ Server/DeviceManagement/DeviceManagement.Application/AssemblyConfigurator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shared.InternalMessaging.CQRS;
+
+namespace ModularHouse.Server.DeviceManagement.Application;
+
+public static class AssemblyConfigurator
+{
+    public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
+    {
+        services.AddCQRS(typeof(AssemblyConfigurator).Assembly);
+        return services;
+    }
+}
