f7d5cbf596921ba861e7e6226663fa202aaa1f24
v.yusupov <v.yusupov@upswot.com>
2023-10-24 13:04:57 +0300

    (fix): Fixed merge conflicts.

--- Server/DeviceManagement/DeviceManagement.Startup/Program.cs
+++ Server/DeviceManagement/DeviceManagement.Startup/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using ModularHouse.Server.DeviceManagement.Api;
 using ModularHouse.Server.DeviceManagement.Application;
+using ModularHouse.Server.DeviceManagement.Domain;
 using ModularHouse.Server.DeviceManagement.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .ConfigureWebApiServices()
     .ConfigureInfrastructureServices(builder.Configuration)
-    .ConfigureApplicationServices();
+    .ConfigureApplicationServices()
+    .ConfigureDomainServices();
 
 var app = builder.Build();
 
