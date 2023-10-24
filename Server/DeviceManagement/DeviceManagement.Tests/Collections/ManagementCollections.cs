0c4a60ab8ef7a13c561f3176ba949ac16ff7fe50
Vladislawv <87234450+Vladislawv@users.noreply.github.com>
2023-10-24 17:21:49 +0300

    Created DeviceManagement.Tests project (#20)
    (feat): Created DeviceManagement.Tests project for tests using in-memory database initialization.

---
+++ Server/DeviceManagement/DeviceManagement.Tests/Collections/ManagementCollections.cs
@@ -0,0 +1,11 @@
+﻿using DeviceManagement.Tests.Fixtures;
+using Xunit;
+
+namespace DeviceManagement.Tests.Collections;
+
+[CollectionDefinition(Constants.ManagementTests)]
+public class ManagementCollections : ICollectionFixture<DbContextFixture>
+{
+    /* This class has no code, and is never created. Its purpose is simply to be the place
+       to apply [CollectionDefinition] and all the ICollectionFixture<> interfaces. */
+}
