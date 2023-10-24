52b90a0ce78c4a5da5232d6819acaac8f7b6a12f
Vladislawv <87234450+Vladislawv@users.noreply.github.com>
2023-10-24 13:05:28 +0300

    Set up EventBus to the DeviceManagement. (#19)
    (feat): Set up internal messaging to the DeviceManagement using DomainEventBus.

---
+++ Server/DeviceManagement/DeviceManagement.Domain/AssemblyConfigurator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shared.InternalMessaging.DomainEvents;
+
+namespace ModularHouse.Server.DeviceManagement.Domain;
+
+public static class AssemblyConfigurator
+{
+    public static IServiceCollection ConfigureDomainServices(this IServiceCollection services)
+    {
+        services.AddDomainEventBus();
+        return services;
+    }
+}
