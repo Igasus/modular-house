0868cfa4a6e27ba8738718bb263a31d0bf2c6637
v.yusupov <v.yusupov@upswot.com>
2023-11-16 18:12:35 +0200

    (feat): Created and migrated User entity.

--- Server/DeviceManagement/DeviceManagement.Infrastructure/DataAccess/Database/PostgreSqlContext.cs
+++ Server/DeviceManagement/DeviceManagement.Infrastructure/DataAccess/Database/PostgreSqlContext.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ModularHouse.Server.DeviceManagement.Domain.UserAggregate;
 
 namespace ModularHouse.Server.DeviceManagement.Infrastructure.DataAccess.Database;
 
 public sealed class PostgreSqlContext : DbContext
 {
     public PostgreSqlContext(DbContextOptions<PostgreSqlContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
 }
