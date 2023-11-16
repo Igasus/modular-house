5c6e2a073b5793ddc8699f47c45d874992eddccf
v.yusupov <v.yusupov@upswot.com>
2023-11-17 00:22:21 +0200

    (fix): Fixed response in CreateUser endpoint.

--- Server/DeviceManagement/DeviceManagement.Api.Http/Controllers/Users/UserController.cs
+++ Server/DeviceManagement/DeviceManagement.Api.Http/Controllers/Users/UserController.cs
@@ -4,7 +4,9 @@ using System.Net.Mime;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using ModularHouse.Libraries.InternalMessaging.DomainEvents.Abstractions;
 using ModularHouse.Server.DeviceManagement.Application.CQRS.Commands;
+using ModularHouse.Server.DeviceManagement.Domain.UserAggregate.Events;
 
 namespace ModularHouse.Server.DeviceManagement.Api.Http.Controllers.Users;
 
@@ -13,10 +15,12 @@ namespace ModularHouse.Server.DeviceManagement.Api.Http.Controllers.Users;
 [Produces(MediaTypeNames.Application.Json)]
 public class UserController : ControllerBase
 {
+    private readonly IDomainEventBus _eventBus;
     private readonly IMediator _mediator;
 
-    public UserController(IMediator mediator)
+    public UserController(IDomainEventBus eventBus, IMediator mediator)
     {
+        _eventBus = eventBus;
         _mediator = mediator;
     }
 
@@ -24,14 +28,17 @@ public class UserController : ControllerBase
     /// Create User with given Id.
     /// </summary>
     /// <param name="id">User Id</param>
-    /// <returns>Result status code</returns>
+    /// <returns>Created User</returns>
     /// <response code = "200">If creation was successful</response>
     /// <response code = "400">If the user already exists in the system</response>
     [HttpPost("{id:guid}")]
     public async Task<IActionResult> CreateAsync([FromRoute, Required] Guid id)
     {
+        var userCreatedTask = _eventBus.WaitAsync<UserCreatedEvent>();
         await _mediator.Send(new CreateUserCommand(id));
-        return Ok();
+
+        var createdUserEvent = await userCreatedTask;
+        return Ok(createdUserEvent.User);
     }
 
     /// <summary>
--- Server/DeviceManagement/DeviceManagement.Application/CQRS/CommandHandlers/CreateUserCommandHandler.cs
+++ Server/DeviceManagement/DeviceManagement.Application/CQRS/CommandHandlers/CreateUserCommandHandler.cs
@@ -3,10 +3,13 @@ using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using ModularHouse.Libraries.InternalMessaging.CQRS.Abstractions;
+using ModularHouse.Libraries.InternalMessaging.DomainEvents.Abstractions;
+using ModularHouse.Server.Common.Domain;
 using ModularHouse.Server.Common.Domain.Exceptions;
 using ModularHouse.Server.DeviceManagement.Application.CQRS.Commands;
 using ModularHouse.Server.DeviceManagement.Application.CQRS.Queries;
 using ModularHouse.Server.DeviceManagement.Domain.UserAggregate;
+using ModularHouse.Server.DeviceManagement.Domain.UserAggregate.Events;
 
 namespace ModularHouse.Server.DeviceManagement.Application.CQRS.CommandHandlers;
 
@@ -14,11 +17,13 @@ public class CreateUserCommandHandler : ICommandHandler<CreateUserCommand>
 {
     private readonly IMediator _mediator;
     private readonly IUserRepository _userRepository;
+    private readonly IDomainEventBus _eventBus;
 
-    public CreateUserCommandHandler(IMediator mediator, IUserRepository userRepository)
+    public CreateUserCommandHandler(IMediator mediator, IUserRepository userRepository, IDomainEventBus eventBus)
     {
         _mediator = mediator;
         _userRepository = userRepository;
+        _eventBus = eventBus;
     }
 
     public async Task Handle(CreateUserCommand request, CancellationToken cancellationToken)
@@ -28,11 +33,14 @@ public class CreateUserCommandHandler : ICommandHandler<CreateUserCommand>
         {
             throw new BadRequestException($"User already exists with given Id: {request.Id}.");
         }
-        
+
         var user = CreateUserFromRequest(request);
 
         await _userRepository.Users.AddAsync(user, cancellationToken);
         await _userRepository.Context.SaveChangesAsync(cancellationToken);
+
+        var userCreatedEvent = new UserCreatedEvent(user, CurrentTransaction.TransactionId);
+        await _eventBus.PublishAsync(userCreatedEvent);
     }
 
     private static User CreateUserFromRequest(CreateUserCommand request)
