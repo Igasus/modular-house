4f898f19332a5900057bf75ba80c861f129e2593
v.yusupov <v.yusupov@upswot.com>
2023-11-18 17:33:28 +0200

    (fix): Moved Converter from Controllers folder to the DataMappers.

---
+++ Server/DeviceManagement/DeviceManagement.Api.Http/DataMappers/Converter.cs
@@ -0,0 +1,12 @@
+﻿using ModularHouse.Server.DeviceManagement.Domain.UserAggregate;
+using ModularHouse.Shared.Models.Responses.DMS;
+
+namespace ModularHouse.Server.DeviceManagement.Api.Http.DataMappers;
+
+public static class Converter
+{
+    public static CreatedUserResponse ToResponse(this User user)
+    {
+        return new CreatedUserResponse(user.Id, user.AdditionDate);
+    }
+}
