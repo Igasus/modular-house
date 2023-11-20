7c96f3ca94e625e7d37296ab939c7050cb741d72
v.yusupov <v.yusupov@upswot.com>
2023-11-20 23:47:14 +0200

    (fix): Removed public modificator from interface methods.

--- Server/DeviceManagement/DeviceManagement.Domain/UserAggregate/IUserDataSource.cs
+++ Server/DeviceManagement/DeviceManagement.Domain/UserAggregate/IUserDataSource.cs
@@ -7,6 +7,6 @@ namespace ModularHouse.Server.DeviceManagement.Domain.UserAggregate;
 
 public interface IUserDataSource
 {
-    public Task<IReadOnlyList<User>> GetAllAsync(CancellationToken cancellationToken);
-    public Task<User> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<IReadOnlyList<User>> GetAllAsync(CancellationToken cancellationToken);
+    Task<User> GetByIdAsync(Guid id, CancellationToken cancellationToken);
 }
--- Server/DeviceManagement/DeviceManagement.Domain/UserAggregate/IUserRepository.cs
+++ Server/DeviceManagement/DeviceManagement.Domain/UserAggregate/IUserRepository.cs
@@ -5,6 +5,6 @@ namespace ModularHouse.Server.DeviceManagement.Domain.UserAggregate;
 
 public interface IUserRepository
 {
-    public Task CreateAsync(User user, CancellationToken cancellationToken);
-    public Task DeleteAsync(User user, CancellationToken cancellationToken);
+    Task CreateAsync(User user, CancellationToken cancellationToken);
+    Task DeleteAsync(User user, CancellationToken cancellationToken);
 }
