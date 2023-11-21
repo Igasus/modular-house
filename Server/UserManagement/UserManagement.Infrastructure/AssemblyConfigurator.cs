312227cbdd2d1c1f5f7f393a1278ac66981bd856
Ihor Hubskyi <ihor.hubskyi@gmail.com>
2023-11-21 11:07:12 +0200

    UMS - Setup Graph DataBase (#38)
    Closes #24
    (feat): UMS - Set up Neo4j GraphDatabase and IDriver service configuration;
    (feat): UMS - Set up User-Secrets.

--- Server/UserManagement/UserManagement.Infrastructure/AssemblyConfigurator.cs
+++ Server/UserManagement/UserManagement.Infrastructure/AssemblyConfigurator.cs
@@ -1,14 +1,29 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using ModularHouse.Server.UserManagement.Domain.UserAggregate;
+using ModularHouse.Server.UserManagement.Infrastructure.ConfigurationOptions;
 using ModularHouse.Server.UserManagement.Infrastructure.DataSources;
 using ModularHouse.Server.UserManagement.Infrastructure.Repositories;
+using Neo4j.Driver;
 
 namespace ModularHouse.Server.UserManagement.Infrastructure;
 
 public static class AssemblyConfigurator
 {
-    public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services)
+    public static IServiceCollection ConfigureInfrastructureServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
     {
+        services.Configure<Neo4jOptions>(configuration.GetSection(Neo4jOptions.Section));
+
+        services.AddScoped(serviceProvider =>
+        {
+            var options = serviceProvider.GetRequiredService<IOptions<Neo4jOptions>>().Value;
+            var authToken = AuthTokens.Basic(options.Auth.UserName, options.Auth.Password);
+            return GraphDatabase.Driver(options.Uri, authToken);
+        });
+        
         services.AddTransient<IUserDataSource, UserDataSource>();
         services.AddTransient<IUserRepository, UserRepository>();
 
--- Server/UserManagement/UserManagement.Infrastructure/DataSources/UserDataSource.cs
+++ Server/UserManagement/UserManagement.Infrastructure/DataSources/UserDataSource.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using ModularHouse.Server.UserManagement.Domain.UserAggregate;
+using Neo4j.Driver;
 
 namespace ModularHouse.Server.UserManagement.Infrastructure.DataSources;
 
 public class UserDataSource : IUserDataSource
 {
-    public Task<IReadOnlyList<User>> GetAllAsync()
+    private readonly IDriver _driver;
+
+    public UserDataSource(IDriver driver)
+    {
+        _driver = driver;
+    }
+
+    public async Task<IReadOnlyList<User>> GetAllAsync()
     {
         throw new NotImplementedException();
     }
 
-    public Task<User> GetByIdAsync()
+    public async Task<User> GetByIdAsync()
     {
         throw new NotImplementedException();
     }
