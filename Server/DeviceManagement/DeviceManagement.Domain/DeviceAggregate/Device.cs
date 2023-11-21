93d5be7c8f2747168ffb315054c84ea6824741dc
v.yusupov <v.yusupov@upswot.com>
2023-11-21 11:48:42 +0200

    (feat): Created and migrated Device entity.

---
+++ Server/DeviceManagement/DeviceManagement.Domain/DeviceAggregate/Device.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ModularHouse.Server.DeviceManagement.Domain.DeviceAggregate;
+
+public class Device
+{
+    public Guid Id { get; set; }
+    public DateTime AdditionDate { get; set; }
+}
--- Server/DeviceManagement/DeviceManagement.Infrastructure/DataAccess/Database/Migrations/20231121094722_Created_Devices.Designer.cs
+++ Server/DeviceManagement/DeviceManagement.Infrastructure/DataAccess/Database/Migrations/20231121094722_Created_Devices.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ModularHouse.Server.DeviceManagement.Infrastructure.DataAccess.Database;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ModularHouse.Server.DeviceManagement.Infrastructure.DataAccess.Database.Migrations
 {
     [DbContext(typeof(PostgreSqlContext))]
-    partial class PostgreSqlContextModelSnapshot : ModelSnapshot
+    [Migration("20231121094722_Created_Devices")]
+    partial class Created_Devices
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
