368155c77a21a4009223c8be41e672703e71e009
v.yusupov <v.yusupov@upswot.com>
2023-11-21 11:59:34 +0200

    (feat): Implemented Device read operations.

---
+++ Server/DeviceManagement/DeviceManagement.Application/DataMappers/DeviceDataMapper.cs
@@ -0,0 +1,13 @@
+﻿using ModularHouse.Server.DeviceManagement.Application.CQRS.QueryResponses;
+using ModularHouse.Server.DeviceManagement.Application.Dto;
+using ModularHouse.Server.DeviceManagement.Domain.DeviceAggregate;
+
+namespace ModularHouse.Server.DeviceManagement.Application.DataMappers;
+
+public static class DeviceDataMapper
+{
+    public static GetDeviceQueryResponse ToResponse(this Device device)
+    {
+        return new GetDeviceQueryResponse(new DeviceDto(device.Id, device.AdditionDate));
+    }
+}
--- Server/DeviceManagement/DeviceManagement.Infrastructure/AssemblyConfigurator.cs
+++ Server/DeviceManagement/DeviceManagement.Infrastructure/AssemblyConfigurator.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ModularHouse.Server.DeviceManagement.Domain.DeviceAggregate;
 using ModularHouse.Server.DeviceManagement.Domain.UserAggregate;
 using ModularHouse.Server.DeviceManagement.Infrastructure.DataAccess.Database;
 using ModularHouse.Server.DeviceManagement.Infrastructure.DataAccess.DataSources;
@@ -12,11 +13,11 @@ namespace ModularHouse.Server.DeviceManagement.Infrastructure;
 public static class AssemblyConfigurator
 {
     private const string POSTGRE_SQL_SERVER = "ModularHousePostgreSqlServer";
-    
+
     public static IServiceCollection ConfigureInfrastructureServices(
         this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<PostgreSqlContext>(options => 
+        services.AddDbContext<PostgreSqlContext>(options =>
             options.UseNpgsql(configuration.GetConnectionString(POSTGRE_SQL_SERVER)));
 
         services
@@ -35,6 +36,8 @@ public static class AssemblyConfigurator
     private static IServiceCollection AddDataSources(this IServiceCollection services)
     {
         services.AddTransient<IUserDataSource, UserDataSource>();
+        services.AddTransient<IDeviceDataSource, DeviceDataSource>();
+
         return services;
     }
 
@@ -42,7 +45,7 @@ public static class AssemblyConfigurator
     {
         using var scope = services.CreateScope();
         var postgreSqlContext = scope.ServiceProvider.GetRequiredService<PostgreSqlContext>();
-        postgreSqlContext.Database.Migrate();;
+        postgreSqlContext.Database.Migrate();
 
         return services;
     }
