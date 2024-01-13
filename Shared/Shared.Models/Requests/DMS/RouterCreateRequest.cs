c7a68a0c7a5ba8d2530efd510d846176fbd7b127
v.yusupov <vladusupov2810@gmail.com>
2024-01-14 00:18:35 +0200

    (feat): Created RouterController.

---
+++ Shared/Shared.Models/Requests/DMS/RouterCreateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModularHouse.Shared.Models.Requests.DMS;
+
+public record RouterCreateRequest
+{
+    public Guid AreaId { get; init; }
+    public Guid DeviceId { get; init; }
+    public Guid UserId { get; init; }
+    public string Name { get; init; }
+    public string Description { get; init; }
+};
---
+++ Shared/Shared.Models/Requests/DMS/RouterUpdateRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ModularHouse.Shared.Models.Requests.DMS;
+
+public record RouterUpdateRequest
+{
+    public Guid UserId { get; init; }
+    public string Name { get; init; }
+    public string Description { get; init; }
+};
