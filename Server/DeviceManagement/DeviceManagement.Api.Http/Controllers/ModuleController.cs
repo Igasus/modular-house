722cc1688d02dc9cc58ccb315cc26697e7d271da
v.yusupov <vladusupov2810@gmail.com>
2024-02-17 01:36:54 +0200

    (feat): Created ModuleController.

---
+++ Server/DeviceManagement/DeviceManagement.Api.Http/Controllers/ModuleController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModularHouse.Libraries.InternalMessaging.CQRS.Abstractions;
+using ModularHouse.Libraries.InternalMessaging.DomainEvents.Abstractions;
+using ModularHouse.Server.Common.Domain;
+using ModularHouse.Server.DeviceManagement.Api.Http.DataMappers;
+using ModularHouse.Server.DeviceManagement.Application.CQRS.Commands;
+using ModularHouse.Server.DeviceManagement.Application.CQRS.Queries;
+using ModularHouse.Server.DeviceManagement.Application.CQRS.QueryResponses;
+using ModularHouse.Server.DeviceManagement.Domain.ModuleAggregate.Events;
+using ModularHouse.Shared.Models.Requests.DMS;
+using ModularHouse.Shared.Models.Responses;
+using ModularHouse.Shared.Models.Responses.DMS;
+
+namespace ModularHouse.Server.DeviceManagement.Api.Http.Controllers;
+
+[ApiController]
+[Route("api/modules")]
+[Produces(MediaTypeNames.Application.Json)]
+public class ModuleController : ControllerBase
+{
+    private readonly IMessageBus _messageBus;
+    private readonly IDomainEventBus _eventBus;
+
+    public ModuleController(IMessageBus messageBus, IDomainEventBus eventBus)
+    {
+        _messageBus = messageBus;
+        _eventBus = eventBus;
+    }
+
+    /// <summary>
+    /// Get all modules.
+    /// </summary>
+    /// <returns>Action result with list of modules</returns>
+    [HttpGet]
+    [ProducesResponseType(typeof(ListedResponse<ModuleResponse>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var getModulesQueryResponse =
+            await _messageBus.Send<GetModulesQuery, GetModulesQueryResponse>(new GetModulesQuery());
+        
+        var response = new ListedResponse<ModuleResponse>(
+            getModulesQueryResponse.Modules.ToResponseList(), getModulesQueryResponse.TotalModulesCount);
+
+        return Ok(response);
+    }
+
+    /// <summary>
+    /// Get Module by id.
+    /// </summary>
+    /// <param name="id">Module id</param>
+    /// <returns>Action result with Module</returns>
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(typeof(ModuleResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetByIdAsync([FromRoute, Required] Guid id)
+    {
+        var getModuleByIdQueryResponse =
+            await _messageBus.Send<GetModuleByIdQuery, GetModuleByIdQueryResponse>(new GetModuleByIdQuery(id));
+
+        return Ok(getModuleByIdQueryResponse.Module.ToResponse());
+    }
+
+    /// <summary>
+    /// Create Module.
+    /// </summary>
+    /// <param name="input">Input data of Module</param>
+    /// <returns>Action result with created Module</returns>
+    [HttpPost]
+    [ProducesResponseType(typeof(ModuleResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> CreateAsync([FromBody, Required] ModuleCreateRequest input)
+    {
+        var moduleCreatedTask = _eventBus.WaitAsync<ModuleCreatedEvent>(transactionId: CurrentTransaction.TransactionId);
+        await _messageBus.Send(new CreateModuleCommand(input.ToDto(), input.RouterId, input.DeviceId));
+
+        var moduleCreatedEvent = await moduleCreatedTask;
+
+        var getModuleByIdQueryResponse = await _messageBus.Send<GetModuleByIdQuery, GetModuleByIdQueryResponse>(
+            new GetModuleByIdQuery(moduleCreatedEvent.ModuleId));
+
+        return Ok(getModuleByIdQueryResponse.Module.ToResponse());
+    }
+
+    /// <summary>
+    /// Update Module.
+    /// </summary>
+    /// <param name="id">Module Id</param>
+    /// <param name="input">Input data of Module</param>
+    /// <returns>Action result with updated Module</returns>
+    [HttpPut("{id:guid}")]
+    [ProducesResponseType(typeof(ModuleResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> UpdateByIdAsync(
+        [FromRoute, Required] Guid id,
+        [FromBody, Required] ModuleUpdateRequest input)
+    {
+        var moduleUpdatedTask = _eventBus.WaitAsync<ModuleUpdatedEvent>(transactionId: CurrentTransaction.TransactionId);
+        await _messageBus.Send(new UpdateModuleByIdCommand(id, input.ToDto()));
+
+        var moduleUpdatedEvent = await moduleUpdatedTask;
+        
+        var getModuleByIdQueryResponse = await _messageBus.Send<GetModuleByIdQuery, GetModuleByIdQueryResponse>(
+            new GetModuleByIdQuery(moduleUpdatedEvent.ModuleId));
+
+        return Ok(getModuleByIdQueryResponse.Module.ToResponse());
+    }
+
+    /// <summary>
+    /// Delete Module.
+    /// </summary>
+    /// <param name="id">Module Id</param>
+    /// <returns>Action result with operation status code</returns>
+    [HttpDelete("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> DeleteByIdAsync([FromRoute, Required] Guid id)
+    {
+        var moduleDeletedTask = _eventBus.WaitAsync<ModuleDeletedEvent>(transactionId: CurrentTransaction.TransactionId);
+        await _messageBus.Send(new DeleteModuleByIdCommand(id));
+
+        await moduleDeletedTask;
+
+        return Ok();
+    }
+}
---
+++ Shared/Shared.Models/Responses/DMS/ModuleResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ModularHouse.Shared.Models.Responses.DMS;
+
+public record ModuleResponse
+{
+    public Guid Id { get; init; }
+    public Guid RouterId { get; init; }
+    public Guid DeviceId { get; init; }
+    public string Name { get; init; }
+    public string Description { get; init; }
+    public DateTime CreationDate { get; init; }
+    public Guid CreatedByUserId { get; init; }
+    public DateTime LastUpdatedDate { get; init; }
+    public Guid LastUpdatedByUserId { get; init; }
+}
